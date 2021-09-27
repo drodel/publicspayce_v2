41939b68b7a02f83508a076315adc1cf3ca25c2f
Dale Rodel <dale.rodel@drodel.com>
2021-09-27 02:15:29 +0200

    crude front page layout with nav icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:fluttericon/linecons_icons.dart';
 
 void main() {
-    runApp(
-        MaterialApp(
-          home: Scaffold (
-            backgroundColor: Colors.blueGrey,
-            appBar: AppBar(
+    runApp( pubSpc() );
+}
+
+class pubSpc extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold (
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
               title: Center(
-                  child: Text('I Am publicSpayce')
-                ),
-                backgroundColor: Colors.blueGrey[900]
-            ),
-            body: Center(
-              child: Image(
-                image: AssetImage('images/pirate_w_heart.png')
+                  child: Text(
+                    'publicSpayce.com',
+                    style: TextStyle(
+                          fontSize: 25,
+                          fontFamily: 'Italianno',
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold
+                    ),
+                  )
               ),
-            )
+              backgroundColor: Colors.blueGrey.shade900
           ),
+          body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children:<Widget> [
+                  Container(
+                    child: Image(
+                      image: AssetImage('images/pirate_w_heart.png')
+                    ),
+                  ),
+                  Container(
+                      width: 100,
+                      height: 70,
+                      color: Colors.blueGrey.shade900,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children:<Widget> [
+                            Container(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children:<Widget> [
+                                  Icon(
+                                    Icons.receipt_long_outlined,
+                                    color: Colors.white
+                                  ),
+                                  Text('About',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'SourceSansPro',
+                                        fontSize: 15
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ), // About Container
+                            Container(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children:<Widget> [
+                                  Icon(
+                                      Icons.person_outlined,
+                                      color: Colors.white
+                                  ),
+                                  Text('Profile',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'SourceSansPro',
+                                        fontSize: 15
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ), // Profile Container
+                            Container(
+                              child: Image(
+                                  image: AssetImage('images/coin_w_jolly_roger.png')
+                              ),
+                            ),
+                            Container(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children:<Widget> [
+                                  Icon(
+                                      Icons.favorite_border_outlined,
+                                      color: Colors.white
+                                  ),
+                                  Text('Your',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'SourceSansPro',
+                                        fontSize: 15
+                                    ),
+                                  ),
+                                  Text('Coins',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'SourceSansPro',
+                                        fontSize: 15
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ), // Your Coins Container
+                            Container(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children:<Widget> [
+                                  Icon(
+                                    Linecons.globe,
+                                    color: Colors.white
+                                  ),
+                                  Text('Public',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'SourceSansPro',
+                                        fontSize: 15
+                                    ),
+                                  ),
+                                  Text('Coins',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'SourceSansPro',
+                                        fontSize: 15
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ), // Public Coins Container
+                          ]
+                      )
+                  ),
+                ]
+            ),
         ),
+      )
     );
+  }
 }
