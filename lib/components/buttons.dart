8e784ec0bfda18051ecc13a9c0ae64fe8f31bc41
Dale Rodel <dale.rodel@drodel.com>
2022-03-04 00:59:26 +0100

    updated all routes to transistion without animation and create no back button

--- lib/components/buttons.dart
+++ lib/components/buttons.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:fluttericon/linecons_icons.dart';
 import 'package:publicspayce_v2/constants.dart';
+import 'package:publicspayce_v2/screens/about.dart';
+import 'package:publicspayce_v2/screens/profile.dart';
+import 'package:publicspayce_v2/screens/public_coins.dart';
+import 'package:publicspayce_v2/screens/user_coins.dart';
 
 TextStyle buttonTextStyle () {
   return const TextStyle(
@@ -10,10 +14,18 @@ TextStyle buttonTextStyle () {
   );
 }
 
+// TODO: make the onTaps a function passing the screen
+
 GestureDetector aboutButton ( BuildContext passedContext ) {
   return GestureDetector(
     onTap: () {
-                 Navigator.pushNamedAndRemoveUntil( passedContext, '/about', (_) => false ); // (_) => false prevents any back button
+//               Navigator.pushNamedAndRemoveUntil( passedContext, '/about', (_) => false ); // (_) => false prevents any back button
+                 Navigator.of(passedContext).pushReplacement(
+                     PageRouteBuilder(
+                         pageBuilder: (_, __, ___) => AboutScreen(),
+                         transitionDuration: Duration.zero
+                     )
+                 ); // (_) => false prevents any back button
               },
     child: Container(
       padding: const EdgeInsets.only(left: kEdgePadding),
@@ -34,7 +46,13 @@ GestureDetector aboutButton ( BuildContext passedContext ) {
 GestureDetector yourProfileButton ( BuildContext passedContext ) {
   return GestureDetector(
     onTap: () {
-                 Navigator.pushNamedAndRemoveUntil( passedContext, '/profile', (_) => false ); // (_) => false prevents any back button
+//                Navigator.pushNamedAndRemoveUntil( passedContext, '/profile', (_) => false ); // (_) => false prevents any back button
+                  Navigator.of(passedContext).pushReplacement(
+                      PageRouteBuilder(
+                          pageBuilder: (_, __, ___) => ProfileScreen(),
+                          transitionDuration: Duration.zero
+                      )
+                  );
               },
     child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -52,11 +70,13 @@ GestureDetector yourProfileButton ( BuildContext passedContext ) {
 GestureDetector yourCoinsButton ( BuildContext passedContext ) {
   return GestureDetector(
     onTap: () {
-               Navigator.pushNamedAndRemoveUntil(
-                       passedContext,
-                       '/mycoins',
-                       (_) => false,  // (_) => false prevents any back button
-               );
+//                Navigator.pushNamedAndRemoveUntil(passedContext, '/mycoins', (_) => false,  // (_) => false prevents any back button*/);
+                  Navigator.of(passedContext).pushReplacement(
+                      PageRouteBuilder(
+                          pageBuilder: (_, __, ___) => UserCoinScreen(),
+                          transitionDuration: Duration.zero
+                      )
+                  );
               },
     child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -75,7 +95,13 @@ GestureDetector yourCoinsButton ( BuildContext passedContext ) {
 GestureDetector publicCoinsButton ( BuildContext passedContext ) {
   return GestureDetector(
     onTap: () {
-                 Navigator.pushNamedAndRemoveUntil( passedContext, '/publiccoins', (_) => false ); // (_) => false prevents any back button
+//                 Navigator.pushNamedAndRemoveUntil( passedContext, '/publiccoins', (_) => false ); // (_) => false prevents any back button
+                   Navigator.of(passedContext).pushReplacement(
+                       PageRouteBuilder(
+                           pageBuilder: (_, __, ___) => PublicCoinsScreen(),
+                           transitionDuration: Duration.zero
+                       )
+                   );
               },
     child: Container(
         padding: const EdgeInsets.only(right: kEdgePadding),
